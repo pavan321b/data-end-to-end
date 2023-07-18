5d52e7347d3c27a4c0913693a84a0d0bc60cadef
M.V.S.Sanjay <48452854+mvssanjay007@users.noreply.github.com>
2023-07-18 22:07:27 +0000

    Visualization using Progress Bars

--- api.py
+++ api.py
@@ -8,7 +8,7 @@ import requests
 import pandas as pd
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 
 import os
@@ -18,8 +18,8 @@ from api_keys import url, headers
 from bert import analyse
 
 
-
 app = Flask(__name__)
+
 
 def text_extract(cnbc_url) -> str:
     """
@@ -37,123 +37,166 @@ def text_extract(cnbc_url) -> str:
 
     return final_text
 
+
 def average_scores(file):
-    file_type=type(file)
+    file_type = type(file)
+    count = 0
     if file_type is not dict:
-        data=json.load(file)
+        data = json.load(file)
     else:
-        data=file
-    avg_scores={'Positive':0,'Neutral':0,'Negative':0}
+        data = file
+    avg_scores = {"Positive": 0, "Neutral": 0, "Negative": 0}
     for i in data.keys():
-            try:
-                int(i)
-                if 'sentiment_score' in data[i].keys():
-                    for j in data[i]['sentiment_score'].keys():
-                        avg_scores[j]+=data[i]['sentiment_score'][j]
-                
-            except:
-                pass
-    return avg_scores
+        try:
+            int(i)
+            if "sentiment_score" in data[i].keys():
+                for j in data[i]["sentiment_score"].keys():
+                    avg_scores[j] += data[i]["sentiment_score"][j]
+                count += 1
+            print('Count: ',count)
+            
+            
+
+        except:
+            pass
+    try:
+        for i in avg_scores.keys():
+            avg_scores[i] /= count
+            avg_scores[i] = 100*round(avg_scores[i], 6)
+        return avg_scores
+    except ZeroDivisionError:
+        return {"Positive": 100 / 3, "Neutral": 100 / 3, "Negative": 100 / 3}
     
+    
+        
+
 
 def API(companies):
-    
-    print('Comapnies: ',companies)
-    
-    
-    html_response={key:{} for key in companies}
-    for company in companies:
-        file_path=f"Data/{company}_news.json"
-        print('Company in search:',company)
-        if os.path.isfile(file_path):
-            print(f'File Already Exists for {company}')
-            json_file=open(file_path,'r',encoding='UTF-8')
-            
-            data=json.load(json_file)
-            json_file.close()
-            if (datetime.now()-datetime.strptime(data['Last_Modified'],"%m-%d-%Y %H:%M:%S")).seconds < 86400:
-    
-                #Extract data from file and return
+    print("Comapnies: ", companies)
 
-                html_response[company]=data_extract(company,file_path,file_exists=True) 
-                
-                
+    html_response = {key: {} for key in companies}
+    for company in companies:
+        file_path = f"Data/{company}_news.json"
+        print("Company in search:", company)
+        file_exists = False
+        if os.path.isfile(file_path):
+            json_file = open(file_path, "r", encoding="UTF-8")
+            if os.path.getsize(file_path) > 0:
+                print(f"File Already Exists for {company}")
+                file_exists = True
+                data = json.load(json_file)
+                if (
+                    datetime.now()
+                    - datetime.strptime(data["Last_Modified"], "%m-%d-%Y %H:%M:%S")
+                ).days < 2:
+                    html_response[company] = data_extract(
+                        company, file_path, file_exists=file_exists
+                    )
+
+                else:
+                    # Extract data from url and update the file
+                    print("Data is old")
+                    html_response[company] = data_extract(
+                        company, file_path, file_exists=file_exists
+                    )
             else:
-                
-                #Extract data from url and update the file
-                
-                html_response[company]=data_extract(company,file_path,file_exists=False) 
+                print("File Size is zero")
+                file_exists = False
+                html_response[company] = data_extract(
+                    company, file_path, file_exists=file_exists
+                )
+
+            json_file.close()
+
         else:
-            #File doesn't exist. So, create one
-            html_response[company]=data_extract(company,file_path,file_exists=False)
+            # File doesn't exist. So, create one
+            html_response[company] = data_extract(
+                company, file_path, file_exists=file_exists
+            )
     return html_response
 
-def data_extract(company,file_path,file_exists=True):
-    '''Data extract from url or file'''
+
+def data_extract(company, file_path, file_exists=True):
+    """Data extract from url or file"""
     if file_exists:
-       
-        with open(file_path,"r",encoding='UTF-8') as jsonfile:
+        with open(file_path, "r", encoding="UTF-8") as jsonfile:
             return average_scores(file=jsonfile)
 
-        
     else:
-
         parameters = ["headline", "url", "shortDateFirstPublished"]
         querystring = {"symbol": company, "page": "1", "pageSize": "30"}
         response = requests.get(url, headers=headers, params=querystring, timeout=10)
-
+        print(response.status_code)
         df = pd.DataFrame.from_dict(response.json())
-        print(f'!!!!!!!!!!!!!!!!!!!!!!!!!!!{company}')
+        print(f"!!!!!!!!!!!!!!!!!!!!!!!!!!!{company}")
         print(df.head())
         with open(file_path, "w+", encoding="UTF-8") as jsonfile:
             dict_dump = {}
             INDEX = 1
-            
+
             for i in df["data"]["symbolEntries"]["results"]:
-                if len(i["tickerSymbols"]) < 3:
+                if len(i["tickerSymbols"]) < 6:
                     if not i["premium"]:
                         if i["__typename"] == "cnbcnewsstory":
                             data = {parameter: i[parameter] for parameter in parameters}
-                            
+
                             data["Text"] = text_extract(i["url"])
-                            if len(data['Text'].split(" "))<300:
+                            print("Length of the text: ", len(data["Text"].split(" ")))
+                            print(data["url"])
+                            if len(data["Text"].split(" ")) <512:
                                 data["sentiment_score"] = analyse(data["Text"])
+                                print(f"{data['sentiment_score']} : {data['url']}")
                             else:
                                 print(f"{company}: {data['url']}")
-                                
+
                             dict_dump[f"{INDEX}"] = data
                             INDEX += 1
 
-            current_time=datetime.now()
-            dict_dump["Last_Modified"]=current_time.strftime("%m-%d-%Y %H:%M:%S")
+            current_time = datetime.now()
+            dict_dump["Last_Modified"] = current_time.strftime("%m-%d-%Y %H:%M:%S")
 
             json.dump(dict_dump, jsonfile, indent=4, separators=(",", ": "))
 
             return average_scores(dict_dump)
 
 
-@app.route('/success/')
+@app.route("/success/")
 def success():
+    """Success data read from index and return to data.html"""
+    data = request.args.getlist("data")
+    stock_names = {
+        "Tesla": "TSLA",
+        "Nvidia": "NVDA",
+        "Intel": "INTC",
+        "AMD": "AMD",
+        "TSMC": "TSMC",
+        "Google": "GOOGL",
+    }
+    data = [stock_names[company] for company in data]
 
-    '''Success data read from index and return to data.html'''
-    data=request.args.getlist('data')
-    stock_names={'Tesla':'TSLA','Nvidia':'NVDA','Intel':'INTC','TSMC':'TSMC'}
-    data=[stock_names[company] for company in data]
+    final_data = API(data)
+
+    print(final_data)
+    for i in final_data.keys():
+        final_data[i]["max"] = {
+            max(final_data[i], key=final_data[i].get): round(
+                final_data[i][max(final_data[i], key=final_data[i].get)], 3
+            )
+        }
+    print(final_data)
+
+    # news_items=[{'Stock_Name':} for data in final_data]
+    return render_template("data.html", news_items=final_data)
 
 
-    final_data=API(data)
-    #news_items=[{'Stock_Name':} for data in final_data]
-    return render_template('data.html', news_items=final_data)
-
-@app.route('/',methods = ['POST', 'GET'])
+@app.route("/", methods=["POST", "GET"])
 def index():
-    '''Route the data appropriately fom index.html'''
-    if request.method == 'POST':
-        selected_companies = request.form.getlist('company')
-        return redirect(url_for('success', data=selected_companies))
-    return render_template('index.html')
+    """Route the data appropriately fom index.html"""
+    if request.method == "POST":
+        selected_companies = request.form.getlist("company")
+        return redirect(url_for("success", data=selected_companies))
+    return render_template("index.html")
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
--- bert.py
+++ bert.py
@@ -1,8 +1,10 @@
-'''
+"""
 Load ML FinBert Model
-'''
+"""
 from transformers import BertTokenizer, BertForSequenceClassification
 from transformers import pipeline
+
+
 def analyse(text):
     """
     # Let's load the model and the tokenizer
@@ -11,18 +13,24 @@ def analyse(text):
     finbert = BertForSequenceClassification.from_pretrained(
         "yiyanghkust/finbert-tone", num_labels=3
     )
-    tokenizer = BertTokenizer.from_pretrained("yiyanghkust/finbert-tone")
+    tokenizer = BertTokenizer.from_pretrained(
+        "yiyanghkust/finbert-tone",
+        truncation=True,
+        max_length=512,
+        padding="max_length",
+    )
+    tokenizer.model_max_length = 512
+    tokens = tokenizer(text, truncation=True, max_length=510)
 
     nlp = pipeline("sentiment-analysis", model=finbert, tokenizer=tokenizer)
-    output = nlp(text)
-    # print(output['label'])
-    print("Model Out: ",output)
-    print(type(output))
-    print(output)
+
+    output = nlp(tokenizer.decode(tokens["input_ids"]))
+
+    print("Model Out: ", output)
     labels = ["Positive", "Neutral", "Negative"]
     final_output = {output[0]["label"]: output[0]["score"]}
     for label in labels:
         if label not in output[0].values():
             final_output[label] = (1 - list(output[0].values())[1]) / 2
-    print('ML: ', final_output)
+    print("ML: ", final_output)
     return final_output
